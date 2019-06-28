ef5f1f90d9bd0773a0e41cf2fa9fe65b9feac5c8
waxime <aveline.maxime@gmail.com>
2019-06-28 18:01:17 +0200

    Add user class

--- User.php
+++ User.php
@@ -1,5 +1,77 @@
 <?php
 
-class User {
+class User
+{
+    protected $email = "";
+    protected $nom = "";
+    protected $prenom = "";
 
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail(string $email): User
+    {
+        if ( !preg_match ( " /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/ " , $email ) ){
+            throw new InvalidArgumentException('L\'email est invalide');
+        }
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     * @return User
+     */
+    public function setNom(string $nom): User
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     * @return User
+     */
+    public function setPrenom(string $prenom): User
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function isValid()
+    {
+        return !($this->isEmpty($this->getNom()) || $this->isEmpty($this->getPrenom()) || $this->isEmpty($this->getEmail()) || $this->isEmpty($this->getAge()));
+    }
+
+    protected function isEmpty($data) {
+        return $data === null || $data === '';
+    }
 }
